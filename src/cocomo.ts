f68d7e513f1c1234c2617c1238b23fd62973530f
Akira TAKAKI <akira.takaki@gmail.com>
2021-05-04 01:42:29 +0900

    三連単のココモ法で 損切りは 15回 に設定。 負け回数14回の累計で資金 200万円 の半分(約100万円)があればいい初期賭け金 1000円 に設定。

--- src/cocomo.ts
+++ src/cocomo.ts
@@ -88,7 +88,7 @@ export async function calcCocomoBet(
 
   let bet: number | null = null;
   try {
-    const defaultBet = 500;
+    const defaultBet = 1000;
 
     const cocomo = readCocomo(type, isSim);
 
