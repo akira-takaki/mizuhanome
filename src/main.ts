4b42e6a8313b181819aae93f874141bfe438c40f
Akira TAKAKI <akira.takaki@gmail.com>
2022-02-05 02:21:01 +0900

    運用開始時間を 08:15 から 08:30 に変更

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,20 @@ import { sendmail } from "#/sendmail";
 async function main(): Promise<void> {
   logger.info("起動");
 
-  // 毎日 08:15 に実行
+  // 毎日 08:30 に実行
   cron.schedule(
-    "0 15 8 * * *",
+    "0 30 8 * * *",
     async () => {
       await boatRace();
     },
     { timezone: "Asia/Tokyo" }
   );
 
-  // 08:15を過ぎて起動されたら処理を始める
-  const startDayjs = dayjs().set("hour", 8).set("minute", 15).set("second", 0);
+  // 08:30を過ぎて起動されたら処理を始める
+  const startDayjs = dayjs().set("hour", 8).set("minute", 30).set("second", 0);
   const nowDayjs = dayjs();
   if (nowDayjs.isAfter(startDayjs)) {
-    logger.info("08:15を過ぎたため実行");
+    logger.info("08:30を過ぎたため実行");
     await boatRace();
   }
 }
